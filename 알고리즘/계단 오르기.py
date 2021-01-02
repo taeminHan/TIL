2470a993ea50295151716101ad7726a41457bef8
taemin <taemin9705@gmail.com>
2021-01-02 23:09:00 +0900

    20210102 계단오르기 포기

---
+++ 알고리즘/계단 오르기.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+num = int(stdin.readline().rstrip())
+floorGroup = []
+
+for i in range(num):
+    floorGroup.append(int(stdin.readline().strip()))
+
+"""
+다른 풀이를 봐도 뭔말인지 모르겠음 그냥 포기
+DP 공부를 일절 안해서 모르겠다.
+DP 공부 후에 재도전 하자
+"""
