69d5f64e61f72f81891f4a1069219c1c4f048607
taemin <taemin9705@gmail.com>
2021-02-04 21:01:26 +0900

    20210203 2920-음계

---
+++ 알고리즘/2920-음계.py
@@ -0,0 +1,13 @@
+import sys
+
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+
+if a == ascending:
+    print('ascending')
+elif a == descending:
+    print('descending')
+else:
+    print('mixed')
