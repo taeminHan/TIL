306472fbb4850f6682c2314b24c37546c6091951
taemin <taemin9705@gmail.com>
2021-02-22 14:46:11 +0900

    20210222 설탕 2839-설탕 배달

---
+++ 알고리즘/2839-설탕 배달.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+cnt = 0
+while n >= 0:
+    if n % 5 == 0:
+        cnt += (n // 5)
+        print(cnt)
+        break
+    n -= 3
+    cnt += 1
+else:
+    print(-1)
