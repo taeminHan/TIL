5c70846942a93b71c65e1b55fde7052f9e840a2b
taemin <taemin9705@gmail.com>
2021-03-19 23:18:08 +0900

    2021-03-19

--- 알고리즘/2798-블랙잭.py
+++ 알고리즘/2798-블랙잭.py
@@ -4,6 +4,15 @@ from collections import deque
 count, num = map(int, sys.stdin.readline().rstrip().split())
 card = deque(map(int, sys.stdin.readline().rstrip().split()))
 
-while True:
+result = deque()
+length = len(card)
 
-print(card)
+for i in range(0, length):
+    for j in range(i+1, length):
+        for k in range(j+1, length):
+            if card[i] + card[j] + card[k] <= num:
+                result.append(card[i] + card[j] + card[k])
+
+result.append(num)
+result = sorted(result)
+print(result[-2])
