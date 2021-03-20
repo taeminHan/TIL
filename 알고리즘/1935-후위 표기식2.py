87797ad2431893460b65af16fb2efef02940dbe2
taemin <taemin9705@gmail.com>
2021-03-21 02:57:44 +0900

    2021-03-21

--- 알고리즘/1935-후위 표기식2.py
+++ 알고리즘/1935-후위 표기식2.py
@@ -1,30 +1,32 @@
 import sys
+from collections import deque
 
-n=int(input())
-str=input()
-nums=[0]*n
-for i in range(n):
-    nums[i]=int(input())
-stack=[]
+num = []
+stack = deque()
+result = 0
 
-for ch in str:
-    #문자이면
-    if ch.isupper():
-        #nums[해당 문자의 아스키코드에 해당하는 index]
-        stack.append(nums[ord(ch)-ord('A')])
-    #연산자이면
-    else:
-        #뒤에 추가된 숫자먼저 빼오고
-        #이전에 추가된 숫자빼오기
-        num2=stack.pop()
-        num1=stack.pop()
-        if ch=='+':
-            stack.append(num1+num2)
-        elif ch=='-':
-            stack.append(num1-num2)
-        elif ch=='/':
-            stack.append(num1/num2)
-        elif ch=='*':
-            stack.append(num1*num2)
-#소수점 두자리까지 출력하는 방법
-print(f"{stack[0]:.2f}")
+n = int(sys.stdin.readline().rstrip())
+
+form = list(sys.stdin.readline().rstrip())
+
+
+for _ in range(n):
+    num.append(int(sys.stdin.readline().rstrip()))
+
+for i in form:
+    if i.isalpha():
+        stack.append(num[ord(i) - 65])
+    elif i == '+':
+        stack.append(stack.pop() + stack.pop())
+    elif i == '-':
+        first_pop = stack.pop()
+        second_pop = stack.pop()
+        stack.append(second_pop - first_pop)
+    elif i == '/':
+        first_pop = stack.pop()
+        second_pop = stack.pop()
+        stack.append(second_pop / first_pop)
+    elif i == '*':
+        stack.append(stack.pop() * stack.pop())
+
+print('{:.2f}'.format(stack[0]))
