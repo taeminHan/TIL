5ee96dd3a267b152c0ee7b6aec906ea1953b0ceb
taemin <taemin9705@gmail.com>
2021-04-09 23:39:51 +0900

    2021-04-09

--- 알고리즘/1699-제곱수의 합.py
+++ 알고리즘/1699-제곱수의 합.py
@@ -1,14 +1,8 @@
 import sys
-import math
 
 n = int(sys.stdin.readline().rstrip())
 
-pow_list = []
-for i in range(1, n + 1):
-    if i ** 2 < n:
-        pow_list.append(i ** 2)
-m = [j for j in range(1, n + 1)]
+# 11 이하 제곱수
+# 1 4 9 11 - 9 2
 
-for i in m:
-    for j in pow_list:
-        if j < n-j:
+
