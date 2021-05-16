fe07e35eb848415f16769a5729c44883478727f3
taemin <taemin9705@gmail.com>
2021-05-16 22:20:32 +0900

    feat: 백준10773
    solved.ac class2 문제 풀기

--- 알고리즘/2675-문자열반복.py
+++ 알고리즘/2675-문자열반복.py
@@ -3,8 +3,8 @@ import sys
 count = int(sys.stdin.readline().rstrip())
 
 for _ in range(count):
-    word_count, word = sys.stdin.readline().rstrip().split()
+    word_count, words = sys.stdin.readline().rstrip().split()
     result = []
-    for i in list(word):
+    for i in words:
         result.append(int(word_count) * i)
     print(''.join(result))
