6679cc534201b749990844ab9b1b2ce8a82db73f
taemin9705@gmail.com <hantaemin@hantaemin-ui-MacBookPro.local>
2022-01-26 18:03:39 +0900

    프로그래머스 행렬의 덧셈

--- 2022년1월/행렬의 덧셈.java
+++ 2022년1월/행렬의 덧셈.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int [arr1.length][arr1[0].length];
         for (int i=0;i < arr1.length;i++){
