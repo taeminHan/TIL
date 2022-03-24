2febafd42a1e88ea1d85918b2ce893231aa653c0
taeminHan <taemin9705@gmail.com>
2022-03-24 22:03:01 +0900

    백준 ??!-10926

---
+++ JavaPS/src/com/boj/boj_10926.java
@@ -0,0 +1,13 @@
+package com.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj_10926 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String id = bufferedReader.readLine();
+        System.out.println(id + "??!");
+    }
+}
